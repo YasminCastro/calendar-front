3d49656c4a26130cfb430f190e2e875747dfb2b3
Yasmin <yasminsdcastro@gmail.com>
2022-10-04 00:48:03 -0300

    feat: seo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Calendar from "../components/Calendar";
 import Header from "../components/Header";
+import SEO from "../components/SEO";
 
 export default function Home() {
   return (
     <>
+      <SEO title="Calendar" />
       <Header />
       <Calendar />
     </>
